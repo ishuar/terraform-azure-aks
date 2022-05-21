4f0877d8a5212d654c24527846efccfe32ffb777
Ishan Sharma <ishansharma887@gmail.com>
2022-05-21 02:54:14 +0200

    test files added to test the module

---
+++ test/aks.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "rg-aks-module-test-euw"
+  location = "West Europe"
+  tags = {
+    type       = "aks-module"
+    managed_by = "terraform"
+  }
+}
+
+module "aks" {
+  source = "../"
+
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  kubernetes_version  = "1.23.5"
+  # private_dns_zone_id = "None"
+  node_resource_group = "rg-node-aks-module-test-euw"
+  dns_prefix          = "exampleaks1"
+}
+
+output "pub_key" {
+  value = module.aks.ssh_pub_key
+}
+output "aks_id" {
+  value = module.aks.aks_id
+}
