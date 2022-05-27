319bc2474fc4551261a77da8101d4ffc41663794
Ishan Sharma <ishansharma8git s>
2022-05-28 01:53:38 +0200

    updated aks module variables with ssh key inputs and fixes

--- variables.tf
+++ variables.tf
@@ -103,9 +103,7 @@ variable "run_command_enabled" {
 variable "tags" {
   type        = map(string)
   description = "(Optional) Tags needs to be attached to the cluster"
-  default = {
-    managed_by = "terraform"
-  }
+  default     = {}
 
 }
 variable "env" {
@@ -485,4 +483,37 @@ variable "log_analytics_workspace_id" {
   type        = string
   description = "(optional) Log analytics where oms agent will push logs"
   default     = null
+}
+###################
+##SSH Key KV Vars ###
+###################
+variable "ssh_key_vault_name" {
+  type        = string
+  description = "(optional) Key Vault name where private key would be stored as password"
+  default     = "kv-ssh-key-aks-module"
+}
+variable "enabled_for_disk_encryption" {
+  type        = bool
+  description = "(optional)Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys."
+  default     = false
+}
+variable "soft_delete_retention_days" {
+  type        = number
+  description = "(optional) he number of days that items should be retained for once soft-deleted."
+  default     = 7
+}
+variable "purge_protection_enabled" {
+  type        = bool
+  description = "(optional) Is Purge Protection enabled for this Key Vault?"
+  default     = false
+}
+variable "expiration_date" {
+  type        = string
+  description = "(optional) Expiration UTC datetime (2024-01-31T00:00:00Z)"
+  default     = "2024-01-31T00:00:00Z"
+}
+variable "ssh_kv_secret" {
+  type        = string
+  description = "(optional) KV secret which contains the ssh ppk generated for aks module"
+  default     = "secret-ppk-aks-module"
 }
