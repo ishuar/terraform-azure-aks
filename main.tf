0f1e424d36bc5e147502317f3fe505c0ad677fc7
Ishan Sharma <is@cloudeteer.de>
2022-12-14 01:04:30 +0100

    updated module with : outputs and existing cluster logic.

--- main.tf
+++ main.tf
@@ -1,204 +1,181 @@
+# ref: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
+#tfsec:ignore:azure-container-logging
+
+data "azurerm_kubernetes_cluster" "aks" {
+  count               = var.existing_aks_cluster ? 1 : 0
+  name                = var.name
+  resource_group_name = var.resource_group_name
+}
+locals {
+  aks_cluster = var.existing_aks_cluster ? data.azurerm_kubernetes_cluster.aks[0] : azurerm_kubernetes_cluster.aks[0]
+}
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                                = var.cluster_name == null ? "aks-cluster-${var.location}-${var.env}" : var.cluster_name
+  count                               = var.existing_aks_cluster ? 0 : 1
+  name                                = var.name
   location                            = var.location
   resource_group_name                 = var.resource_group_name
   kubernetes_version                  = var.kubernetes_version
-  node_resource_group                 = var.node_resource_group
-  dns_prefix                          = try(var.dns_prefix, null)
-  dns_prefix_private_cluster          = var.dns_prefix == null ? var.dns_prefix_private_cluster : null
+  dns_prefix                          = var.dns_prefix
+  dns_prefix_private_cluster          = var.dns_prefix_private_cluster
   automatic_channel_upgrade           = var.automatic_channel_upgrade
-  sku_tier                            = var.sku_tier
+  api_server_authorized_ip_ranges     = var.api_server_authorized_ip_ranges
+  azure_policy_enabled                = var.azure_policy_enabled
+  disk_encryption_set_id              = var.disk_encryption_set_id
+  local_account_disabled              = var.local_account_disabled
+  node_resource_group                 = var.node_resource_group
   private_cluster_enabled             = var.private_cluster_enabled
   private_dns_zone_id                 = var.private_dns_zone_id
+  public_network_access_enabled       = var.public_network_access_enabled
   private_cluster_public_fqdn_enabled = var.private_cluster_public_fqdn_enabled
-  api_server_authorized_ip_ranges     = toset(var.api_server_authorized_ip_ranges)
-  azure_policy_enabled                = var.azure_policy_enabled
-  http_application_routing_enabled    = var.http_application_routing_enabled
-  local_account_disabled              = var.local_account_disabled
-  disk_encryption_set_id              = var.disk_encryption_set_id
-  open_service_mesh_enabled           = var.open_service_mesh_enabled
   role_based_access_control_enabled   = var.role_based_access_control_enabled
+  sku_tier                            = var.sku_tier
   run_command_enabled                 = var.run_command_enabled
-
-  tags = merge(var.tags,
-    {
-      env        = var.env
-      managed_by = "terraform"
-    },
-  )
-
+  oidc_issuer_enabled                 = var.oidc_issuer_enabled
+  http_application_routing_enabled    = var.http_application_routing_enabled
+  open_service_mesh_enabled           = var.open_service_mesh_enabled
+  tags                                = var.tags
   network_profile {
-    network_mode       = var.net_profile_network_mode
-    network_plugin     = var.net_profile_network_policy == "azure" ? "azure" : var.net_profile_network_plugin
-    network_policy     = var.net_profile_network_policy
-    dns_service_ip     = var.net_profile_docker_bridge_cidr != null && var.net_profile_service_cidr != null ? var.net_profile_dns_service_ip : null
-    docker_bridge_cidr = var.net_profile_dns_service_ip != null && var.net_profile_service_cidr != null ? var.net_profile_docker_bridge_cidr : null
-    outbound_type      = var.net_profile_outbound_type
-    pod_cidr           = var.net_profile_network_plugin == "kubenet" ? var.net_profile_pod_cidr : null
-    service_cidr       = var.net_profile_dns_service_ip != null && var.net_profile_docker_bridge_cidr != null && var.net_profile_network_plugin == "kubenet" ? var.net_profile_service_cidr : null
+    network_plugin     = var.network_plugin
+    network_policy     = var.network_policy
+    pod_cidr           = var.pod_cidr
+    service_cidr       = var.service_cidr
+    dns_service_ip     = var.dns_service_ip
+    docker_bridge_cidr = var.docker_bridge_cidr
+    outbound_type      = var.outbound_type
   }
 
-  linux_profile {
-    admin_username = var.admin_username
-    ssh_key {
-      # remove any new lines using the replace interpolation function
-      key_data = replace(var.public_ssh_key, "\n", "")
+  default_node_pool {
+    name                         = var.default_node_pool_name
+    vm_size                      = var.default_node_pool_vm_size
+    node_count                   = var.default_node_pool_node_count
+    enable_auto_scaling          = var.default_node_pool_enable_auto_scaling
+    enable_host_encryption       = var.default_node_pool_enable_host_encryption
+    enable_node_public_ip        = var.default_node_pool_enable_node_public_ip
+    kubelet_disk_type            = var.default_node_pool_kubelet_disk_type
+    node_public_ip_prefix_id     = var.default_node_pool_public_ip_prefix_id
+    node_labels                  = merge({ "aks_cluster_name" = var.name }, var.default_node_pool_node_labels)
+    only_critical_addons_enabled = var.default_node_pool_only_critical_addons_enabled
+    os_disk_size_gb              = var.default_node_pool_os_disk_size_gb
+    os_disk_type                 = var.default_node_pool_os_disk_type
+    os_sku                       = var.default_node_pool_os_sku
+    tags                         = merge({ "aks_cluster_name" = var.name }, var.default_node_pool_tags, var.tags)
+    ultra_ssd_enabled            = var.default_node_pool_ultra_ssd_enabled
+    vnet_subnet_id               = var.vnet_subnet_id
+    min_count                    = var.default_node_pool_min_count
+    max_count                    = var.default_node_pool_max_count
+    fips_enabled                 = var.default_node_pool_fips_enabled
+    upgrade_settings {
+      max_surge = var.default_node_pool_upgrade_max_surge
     }
   }
-
-  dynamic "auto_scaler_profile" {
-    for_each = var.custom_auto_scaler_profile ? [1] : []
-    content {
-      balance_similar_node_groups      = var.balance_similar_node_groups
-      expander                         = var.expander
-      max_graceful_termination_sec     = var.max_graceful_termination_sec
-      max_node_provisioning_time       = var.max_node_provisioning_time
-      max_unready_nodes                = var.max_unready_nodes
-      max_unready_percentage           = var.max_unready_percentage
-      new_pod_scale_up_delay           = var.new_pod_scale_up_delay
-      scale_down_delay_after_add       = var.scale_down_delay_after_add
-      scale_down_delay_after_delete    = try(var.scale_down_delay_after_delete, var.scan_interval)
-      scale_down_delay_after_failure   = var.scale_down_delay_after_failure
-      scan_interval                    = var.scan_interval
-      scale_down_unneeded              = var.scale_down_unneeded
-      scale_down_unready               = var.scale_down_unready
-      scale_down_utilization_threshold = var.scale_down_utilization_threshold
-      empty_bulk_delete_max            = var.empty_bulk_delete_max
-      skip_nodes_with_local_storage    = var.skip_nodes_with_local_storage
-      skip_nodes_with_system_pods      = var.skip_nodes_with_system_pods
-    }
-  }
-
   dynamic "azure_active_directory_role_based_access_control" {
-    for_each = var.managed_ad_rbac == true ? [1] : []
+    for_each = var.aad_rbac_enabled ? [1] : []
     content {
-      managed                = var.managed_ad_rbac
-      tenant_id              = var.tenant_id
-      admin_group_object_ids = var.managed_ad_rbac == true ? var.admin_group_object_ids : null
-      azure_rbac_enabled     = var.managed_ad_rbac == true ? var.azure_rbac_enabled : null
-      client_app_id          = var.managed_ad_rbac == false ? var.client_app_id : null
-      server_app_id          = var.managed_ad_rbac == false ? var.server_app_id : null
-      server_app_secret      = var.managed_ad_rbac == false ? var.server_app_secret : null
+      managed = var.aad_rbac_managed
+
+      #if managed
+      admin_group_object_ids = var.aad_rbac_managed ? var.aad_rbac_managed_admin_group_ids : null
+      azure_rbac_enabled     = var.aad_rbac_managed ? var.aad_azure_rbac_enabled : null
+
+      # if not managed
+      client_app_id     = var.aad_rbac_managed ? null : var.aad_rbac_unmanaged_client_app_id
+      server_app_id     = var.aad_rbac_managed ? null : var.aad_rbac_unmanaged_server_app_id
+      server_app_secret = var.aad_rbac_managed ? null : var.aad_rbac_unmanaged_server_app_secret
     }
   }
-
-  dynamic "default_node_pool" {
-    for_each = var.enable_auto_scaling == true ? [] : ["default_node_pool_manually_scaled"]
+  dynamic "http_proxy_config" {
+    for_each = var.http_proxy_config != null ? [1] : []
     content {
-      orchestrator_version         = var.kubernetes_version == "" ? var.orchestrator_version : var.kubernetes_version
-      name                         = var.node_pool_name
-      node_count                   = var.node_count
-      vm_size                      = var.vm_size
-      os_disk_size_gb              = var.os_disk_size_gb
-      vnet_subnet_id               = var.vnet_subnet_id
-      enable_auto_scaling          = var.enable_auto_scaling
-      node_public_ip_prefix_id     = var.enable_node_public_ip ? var.node_public_ip_prefix_id : null
-      enable_node_public_ip        = var.enable_node_public_ip
-      node_labels                  = var.node_labels
-      type                         = var.agents_type
-      only_critical_addons_enabled = var.only_critical_addons_enabled
-      kubelet_disk_type            = var.kubelet_disk_type
-      tags = merge(var.tags,
-        {
-          env = var.env
-        },
-      var.agents_tags)
-      max_pods               = var.max_pods
-      enable_host_encryption = var.enable_host_encryption
-      dynamic "kubelet_config" {
-        for_each = var.custom_kubelet_config ? [1] : []
-        content {
-          container_log_max_line    = var.container_log_max_line
-          container_log_max_size_mb = var.container_log_max_size_mb
-        }
-      }
-      dynamic "linux_os_config" {
-        for_each = var.swap_file_size_mb != null ? [1] : []
-        content {
-          swap_file_size_mb = var.swap_file_size_mb
-        }
-      }
+      https_proxy = var.http_proxy_config.https_proxy
+      http_proxy  = var.http_proxy_config.http_proxy
+      no_proxy    = var.http_proxy_config.no_proxy
     }
   }
-
-  dynamic "default_node_pool" {
-    for_each = var.enable_auto_scaling == true ? ["default_node_pool_auto_scaled"] : []
+  dynamic "ingress_application_gateway" {
+    for_each = var.ingress_app_gw_enabled ? [1] : []
     content {
-      max_count                    = var.max_count
-      min_count                    = var.min_count
-      orchestrator_version         = var.kubernetes_version == "" ? var.orchestrator_version : var.kubernetes_version
-      name                         = var.node_pool_name
-      node_count                   = null
-      vm_size                      = var.vm_size
-      os_disk_size_gb              = var.os_disk_size_gb
-      vnet_subnet_id               = var.vnet_subnet_id
-      enable_auto_scaling          = var.enable_auto_scaling
-      node_public_ip_prefix_id     = var.enable_node_public_ip ? var.node_public_ip_prefix_id : null
-      enable_node_public_ip        = var.enable_node_public_ip
-      node_labels                  = var.node_labels
-      type                         = var.agents_type
-      only_critical_addons_enabled = var.only_critical_addons_enabled
-      kubelet_disk_type            = var.kubelet_disk_type
-      tags = merge(var.tags,
-        {
-          env = var.env
-        },
-      var.agents_tags)
-      max_pods               = var.max_pods
-      enable_host_encryption = var.enable_host_encryption
-      dynamic "kubelet_config" {
-        for_each = var.custom_kubelet_config ? [1] : []
-        content {
-          container_log_max_line    = var.container_log_max_line
-          container_log_max_size_mb = var.container_log_max_size_mb
-        }
-      }
-      dynamic "linux_os_config" {
-        for_each = var.swap_file_size_mb != null ? [1] : []
-        content {
-          swap_file_size_mb = var.swap_file_size_mb
-        }
-      }
+      gateway_id   = var.ingress_app_gw_id != null ? var.ingress_app_gw_id : null
+      gateway_name = var.ingress_app_gw_name
+
+      # if a new gw should be created
+      subnet_cidr = var.ingress_app_gw_id == null ? var.ingress_app_gw_subnet_cidr : null
+      subnet_id   = var.ingress_app_gw_id == null ? var.ingress_app_gw_subnet_id : null
     }
   }
-
-  dynamic "service_principal" {
-    for_each = var.client_id != "" && var.client_secret != "" ? ["service_principal"] : []
-    content {
-      client_id     = var.client_id
-      client_secret = var.client_secret
-    }
-  }
-
-  dynamic "identity" {
-    for_each = var.client_id == "" || var.client_secret == "" ? ["identity"] : []
-    content {
-      type         = var.identity_type
-      identity_ids = var.identity_ids
-    }
-  }
-
   dynamic "key_vault_secrets_provider" {
-    for_each = var.secret_rotation_enabled != null ? ["KeyVaultSecretProvider"] : []
+    for_each = var.key_vault_secrets_provider_enabled ? [1] : []
     content {
-      secret_rotation_enabled  = var.secret_rotation_enabled
-      secret_rotation_interval = var.secret_rotation_interval
+      secret_rotation_enabled  = var.key_vault_secrets_provider_secret_rotation_enabled
+      secret_rotation_interval = var.key_vault_secrets_provider_secret_rotation_interval
     }
   }
-
   dynamic "kubelet_identity" {
-    for_each = var.enable_kublet_identity ? ["kubelet_identiry"] : []
+    for_each = var.kubelet_identity_enabled ? [1] : []
     content {
-      client_id                 = var.kublet_client_id
-      object_id                 = var.kubelet_object_id
-      user_assigned_identity_id = var.kubelet_user_assigned_identity_id
+      client_id                 = var.kubelet_identity_client_id
+      object_id                 = var.kubelet_identity_object_id
+      user_assigned_identity_id = var.kubelet_identity_user_assigned_identity_id
     }
   }
-
-  dynamic "oms_agent" {
-    for_each = var.log_analytics_workspace_id != null ? ["oms_agent"] : []
+  dynamic "windows_profile" {
+    for_each = var.windows_profile_enabled ? [1] : []
+    content {
+      admin_username = var.windows_profile_admin_username
+      admin_password = var.windows_profile_admin_password
+      license        = var.windows_profile_license
+    }
+  }
+  dynamic "microsoft_defender" {
+    for_each = var.enable_microsoft_defender ? [1] : []
     content {
       log_analytics_workspace_id = var.log_analytics_workspace_id
     }
   }
+  identity {
+    type         = var.identity_type
+    identity_ids = var.identity_ids
+  }
+
+}
+
+# ref: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster_node_pool
+resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
+  for_each                 = var.additional_node_pools
+  name                     = try(each.value["os_type"], null) == "Windows" ? format("%s%s", substr(each.key, 0, 3), substr(each.key, -3, -1)) : each.key
+  kubernetes_cluster_id    = var.existing_aks_cluster ? data.azurerm_kubernetes_cluster.aks[0].id : azurerm_kubernetes_cluster.aks[0].id
+  vm_size                  = each.value["vm_size"]
+  enable_auto_scaling      = try(each.value["enable_auto_scaling"], null)
+  enable_host_encryption   = try(each.value["enable_host_encryption"], null)
+  enable_node_public_ip    = try(each.value["enable_node_public_ip"], null)
+  eviction_policy          = try(each.value["priority"], null) == "Spot" ? try(each.value["eviction_policy"], null) : null
+  mode                     = try(each.value["mode"], null)
+  node_labels              = merge({ "aks_cluster_name" = var.name }, (try(each.value["node_labels"], null)))
+  node_public_ip_prefix_id = try(each.value["enable_node_public_ip"], false) ? try(each.value["node_public_ip_prefix_id"], null) : null
+  node_taints              = try(each.value["node_taints"], null)
+  orchestrator_version     = var.existing_aks_cluster ? each.value["orchestrator_version"] : var.kubernetes_version
+  os_disk_size_gb          = try(each.value["os_disk_size_gb"], null)
+  os_disk_type             = try(each.value["os_disk_type"], null)
+  pod_subnet_id            = try(each.value["pod_subnet_id"], null)
+  os_type                  = try(each.value["os_type"], null)
+  scale_down_mode          = try(each.value["scale_down_mode"], null)
+  ultra_ssd_enabled        = try(each.value["ultra_ssd_enabled"], null)
+  vnet_subnet_id           = var.vnet_subnet_id
+  zones                    = try(each.value["zones"], null)
+  node_count               = each.value["node_count"]
+  max_count                = try(each.value["enable_auto_scaling"], false) ? each.value["max_count"] : null
+  min_count                = try(each.value["enable_auto_scaling"], false) ? each.value["min_count"] : null
+  dynamic "upgrade_settings" {
+    for_each = try(each.value.upgrade_settings, {})
+    content {
+      max_surge = upgrade_settings.value["max_surge"]
+    }
+  }
+
+  tags = merge(
+    {
+      "aks_cluster_name" = var.name
+    },
+    var.tags,
+    try(each.value["node_pool_tags"], null)
+  )
 }
